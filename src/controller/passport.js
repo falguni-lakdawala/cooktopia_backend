b9b64003cb0f4b8024884dbe404f3e694818093d
Maneesh Thouti <33095871+Maneesh43@users.noreply.github.com>
2021-07-16 10:08:30 -0700

    Update passport.js

--- src/controller/passport.js
+++ src/controller/passport.js
@@ -1,91 +1,91 @@
-// import { Strategy } from 'passport-google-oauth20';
-// import  mongoose   from 'mongoose';
-// import { UserSchema } from '../models/userModel.js';
-// import { config } from '../assets/config.js';
+import { Strategy } from 'passport-google-oauth20';
+import  mongoose   from 'mongoose';
+import { UserSchema } from '../models/userModel.js';
+import { config } from '../assets/config.js';
 
-// const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
 
-// export const passports = (passport) =>{
+export const passports = (passport) =>{
     
-//     passport.serializeUser(function(user, cb) {
-//         cb(null, user);
-//     });
+    passport.serializeUser(function(user, cb) {
+        cb(null, user);
+    });
     
-//     passport.deserializeUser(function(user, cb) {
-//         User.findOne(user.id, function (err, user) {
-//             cb(null, user);
-//         });
-//     });
+    passport.deserializeUser(function(user, cb) {
+        User.findOne(user.id, function (err, user) {
+            cb(null, user);
+        });
+    });
     
-//     passport.use(
-//         new Strategy({
-//             clientID: config.googleClientID,
-//             clientSecret: config.clientSecret,
-//             callbackURL: 'http://localhost:5000/login/callback'
-//         }, 
-//         async (accessToken, refreshToken, profile, done) => {
+    passport.use(
+        new Strategy({
+            clientID: config.googleClientID,
+            clientSecret: config.clientSecret,
+            callbackURL: 'http://localhost:5000/login/callback'
+        }, 
+        async (accessToken, refreshToken, profile, done) => {
     
-//             let user = await User.findOne({email : profile.emails[0].value});
-//             if(!user){
+            let user = await User.findOne({email : profile.emails[0].value});
+            if(!user){
     
-//                     const newUser = new User({
-//                         googleId: profile.id,
-//                         displayName : profile.displayName,
-//                         firstName : profile.name[1],
-//                         lastName : profile.name[0],
-//                         email : profile.emails[0].value,
-//                         image : profile.photos[0].value
-//                     });
+                    const newUser = new User({
+                        googleId: profile.id,
+                        displayName : profile.displayName,
+                        firstName : profile.name[1],
+                        lastName : profile.name[0],
+                        email : profile.emails[0].value,
+                        image : profile.photos[0].value
+                    });
     
-//                     await newUser.save();
-//                     done(null, newUser);
-//                 }
-//                 else{
-//                     done(null, user);
-//                 }
-//         })
-//       );    
+                    await newUser.save();
+                    done(null, newUser);
+                }
+                else{
+                    done(null, user);
+                }
+        })
+      );    
     
-// }
+}
 
-// export const getUserInfo = (req, res) => {
-//     // console.log('url ' + config.apiURL + '/complexSearch?apiKey=' + config.apiKey + '&cuisine=American');
+export const getUserInfo = (req, res) => {
+    // console.log('url ' + config.apiURL + '/complexSearch?apiKey=' + config.apiKey + '&cuisine=American');
   
-//     if(req.session != null && req.session != undefined && req.session.user != undefined){
-//       console.log('current user : ' + req.session.user.email);
+    if(req.session != null && req.session != undefined && req.session.user != undefined){
+      console.log('current user : ' + req.session.user.email);
       
-//       User.find({ email: req.session.user.email },
-//         (err, user) => {
-//         if (err) {
-//           res.send(err);
-//         }
-//         res.json(user);
-//       });
+      User.find({ email: req.session.user.email },
+        (err, user) => {
+        if (err) {
+          res.send(err);
+        }
+        res.json(user);
+      });
       
-//     }
-//     else
-//     {
-//         res.send('null');
-//     }
-// };
+    }
+    else
+    {
+        res.send('null');
+    }
+};
 
-// export const createUser = (req, res) =>{
-//     let user = await User.findOne({email : req.body.email });
-//     if(!user){
-//         const newUser = new User({
-//             userID: req.body.id,
-//             displayName : req.body.displayName,
-//             firstName : req.body.fname,
-//             lastName : req.body.lname,
-//             email : req.body.email,
-//             image : req.body.photoURL
-//         });
+export const createUser = (req, res) =>{
+    let user = await User.findOne({email : req.body.email });
+    if(!user){
+        const newUser = new User({
+            userID: req.body.id,
+            displayName : req.body.displayName,
+            firstName : req.body.fname,
+            lastName : req.body.lname,
+            email : req.body.email,
+            image : req.body.photoURL
+        });
 
-//         await newUser.save();
-//         res.send(newUser);
-//     }
-//     else{
-//         res.send(user);
-//     }
-// };
+        await newUser.save();
+        res.send(newUser);
+    }
+    else{
+        res.send(user);
+    }
+};
 
